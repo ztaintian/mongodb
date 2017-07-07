80e3e3aa78785e6fc7d934751556077a189bf309
ztiantian <243752773@qq.com>
2017-07-07 17:52:04 +0800

    7/7

--- app.js
+++ app.js
@@ -1,1 +1,26 @@
 import express from 'express'
+import config from 'config-lite'
+const app = express();
+console.log(config.url)
+// const port = '3000';
+app.get('/', function(req, res){
+  res.send('hello world');
+});
+app.all('*', (req, res, next) => {
+	res.header("Access-Control-Allow-Origin", req.headers.origin || '*');
+	res.header("Access-Control-Allow-Headers", "Content-Type, Authorization, X-Requested-With");
+	res.header("Access-Control-Allow-Methods", "PUT,POST,GET,DELETE,OPTIONS");
+  	res.header("Access-Control-Allow-Credentials", true); //可以带cookies
+	res.header("X-Powered-By", '3.2.1')
+	if (req.method == 'OPTIONS') {
+	  	res.send(200);
+	} else {
+	    next();
+	}
+});
+
+app.listen(3000,() =>{
+	// console.log(`listen on port ${port}`)
+})
+
+// kk
---
+++ config/default.js
@@ -0,0 +1,16 @@
+
+'use strict';
+
+module.exports = {
+	port: 3000,
+	url: 'mongodb://localhost:27017/elm',
+	session: {
+		name: 'SID',
+		secret: 'SID',
+		cookie: {
+			httpOnly: true,
+		    secure:   false,
+		    maxAge:   365 * 24 * 60 * 60 * 1000,
+		}
+	}
+}
