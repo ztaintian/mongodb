7bccad3f82910729459b2b8c06bc7d1e75a74a0c
ztiantian <243752773@qq.com>
2017-07-10 23:00:52 +0800

    7/10

--- controller/admin.js
+++ controller/admin.js
@@ -1,4 +1,6 @@
 import AdminModdel from '../models/admin';
+import formidable from 'formidable';
+import dtime from 'time-formater';
 
 class Admin extends AddressComponent {
 	constructor(){
@@ -6,6 +8,15 @@ class Admin extends AddressComponent {
 		this.login = this.login.bind(this)
 		this.encryption = this.encryption.bind(this)
 		this.register = this.register.bind(this)
+		this.updateAvatar = this.updateAvatar.bind(this)
+	}
+	encryption(password){
+		const newpassword = this.Md5(this.Md5(password).substr(2,7)+this.Md5(password))
+		return newpassword
+	}
+	Md5(password){
+		const md5 = crypto.createHash('md5')
+		return md5.update(password).digest('base64')
 	}
 	async login(req,res,next){
 		const form = new formidadle.IncomingFrom();
@@ -32,6 +43,53 @@ class Admin extends AddressComponent {
 					message:err.message,
 				})
 				return;
+			}
+			try{
+				const admin = await AdminModel.findOne({useName})
+				if(!admin){
+					res.send({
+						status:0,
+						type:'USER_HAS_EXIST',
+						message:'用户已经存在',
+					})
+				}else{
+					const adminTip = status == 1? '管理员':'超级管理员'
+					const admin_id = await this.getId('admin_id');
+					const cityInfo = await this.guessPosition(req);
+					const newAdmin = {
+						useName,
+						password:newpassword,
+						id:admin_id,
+						create_time:dtime().format('YYYY-MM-DD HH:mm'),
+						admin:adminTip,
+						status,
+						city:cityInfo.city
+					}
+					await AdminModel.create(newAdmin)
+					req.session.admin_id = admin_id;
+					res.send({
+						status:1,
+						success:'注册管理员成功',
+					})
+				}else if(newpassword.tiString()!= admin.password.toString()){
+					res.send({
+						status:0,
+						type:'ERROR_PASSWORD',
+						message:'该用户已存在，密码输入错误',
+					})
+				}else{
+					req.session.admin_id = admin.id;
+					res.send({
+						status:1,
+						success:'登录成功',
+					})
+				}
+			}catch(err){
+				res.send({
+					status:0,
+					type:'LOGIN_ADMIN_FAILED',
+					message:'登录管理员失败',
+				})
 			}
 		})
 	}
@@ -63,7 +121,7 @@ class Admin extends AddressComponent {
 			}
 			try{
 				const admin = await AdminModel.findOne({useName})
-				if(!admin){
+				if(admin){
 					res.send({
 						status:0,
 						type:'USER_HAS_EXIST',
@@ -72,7 +130,7 @@ class Admin extends AddressComponent {
 				}else{
 					const adminTip = status == 1? '管理员':'超级管理员'
 					const admin_id = await this.getId('admin_id');
-					const cityInfo = await this.guessPosition(req);
+					const newpassword = await this.encryption(password);
 					const newAdmin = {
 						useName,
 						password:newpassword,
@@ -80,32 +138,19 @@ class Admin extends AddressComponent {
 						create_time:dtime().format('YYYY-MM-DD HH:mm'),
 						admin:adminTip,
 						status,
-						city:cityInfo.city
-					} 
+					}
 					await AdminModel.create(newAdmin)
 					req.session.admin_id = admin_id;
 					res.send({
 						status:1,
-						success:'注册管理员成功',
-					})
-				}else if(newpassword.tiString()!= admin.password.toString()){
-					res.send({
-						status:0,
-						type:'ERROR_PASSWORD',
-						message:'该用户已存在，密码输入错误',
-					})
-				}else{
-					req.session.admin_id = admin.id;
-					res.send({
-						status:1,
-						success:'登录成功',
+						message:'注册管理员成功',
 					})
 				}
 			}catch(err){
 				res.send({
 					status:0,
-					type:'LOGIN_ADMIN_FAILED',
-					message:'登录管理员失败',
+					type:'REGISTER_ADMIN_FAILED',
+					message:'注册管理员失败',
 				})
 			}
 		})
